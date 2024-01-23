26e1d1ee33664e914f868abbd552ff0b096c3bc1
Rafael Costa <44804@tjmt.jus.br>
2024-01-22 21:26:50 -0400

    feat: created login page

--- lib/features/login/login_page.dart
+++ lib/features/login/login_page.dart
@@ -1,15 +1,102 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
   @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Stack(children: [
-          Text("COTA Energy")
-        ],),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "COTA Energy",
+              style: TextStyle(
+                  fontSize: 35,
+                  color: Colors.blue,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              "Welcome!",
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 80,
+            ),
+            SizedBox(
+                width: MediaQuery.of(context).size.width - 40,
+                child: const TextField(
+                    decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20))),
+                  labelText: 'E-mail',
+                ))),
+            const SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+                width: MediaQuery.of(context).size.width - 40,
+                child: const TextField(
+                    decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20))),
+                  labelText: 'Password',
+                ))),
+            Align(
+              alignment: Alignment.centerRight,
+              child: Padding(
+                padding: const EdgeInsets.only(right: 20),
+                child: TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Forgot password",
+                      style: TextStyle(color: Colors.grey.shade700),
+                    )),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            SizedBox(
+                width: MediaQuery.of(context).size.width - 40,
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    padding: const EdgeInsets.all(15),
+                  ),
+                  onPressed: () => {},
+                  child: const Text(
+                    "Login",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                )),
+            const SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width - 200,
+              child: TextButton(
+                onPressed: () {},
+                child: Text(
+                  "Register",
+                  style: TextStyle(color: Colors.grey.shade700),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:cota_energy_flutter/app_widget.dart';
 import 'package:cota_energy_flutter/features/home/home_page.dart';
+import 'package:cota_energy_flutter/features/login/login_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
@@ -10,11 +11,12 @@ void main() {
 class AppModule extends Module {
   @override
   void binds(i){
-
+    Modular.setInitialRoute('/login');
   }
 
   @override
-  void routes(r){
-    r.child('/', child: (context) => const HomePage());
+  void routes(r) {
+    r.child('/home', child: (context) => const HomePage());
+    r.child('/login', child: (context) => const LoginPage());
   }
 }
