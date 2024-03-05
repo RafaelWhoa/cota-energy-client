c2442fe6efd23a80a0bc101d42ef7061b576ce19
Rafael Costa <44804@tjmt.jus.br>
2024-03-05 18:12:04 -0400

    feat: add circular progress indicator on login button

--- lib/features/login/login_page.dart
+++ lib/features/login/login_page.dart
@@ -19,23 +19,36 @@ final TextEditingController _passwordController = TextEditingController();
 const String apiIP = 'localhost:8080';
 
 Future<String?> _login(String email, String password) async {
-  var res = await http.post(Uri.http(apiIP, "/auth/login"), body: json.encode({
-    "email": email,
-    "password": password
-  }), headers: {"accept": "application/json", "content-type": "application/json" });
+  var res = await http.post(Uri.http(apiIP, "/auth/login"),
+      body: json.encode({"email": email, "password": password}),
+      headers: {
+        "accept": "application/json",
+        "content-type": "application/json"
+      });
   return res.body;
 }
 
 void displayDialog(context, title, text) => showDialog(
-  context: context,
-  builder: (context) =>
-      AlertDialog(
-          title: Text(title),
-          content: Text(text)
-      ),
-);
+      context: context,
+      builder: (context) =>
+          AlertDialog(title: Text(title), content: Text(text)),
+    );
 
 class _LoginPageState extends State<LoginPage> {
+  @override
+  void initState() {
+    super.initState();
+    _isLoginDisabled = false;
+  }
+
+  void _toggleLoginButton() {
+    setState(() {
+      _isLoginDisabled = !_isLoginDisabled;
+    });
+  }
+
+  bool _isLoginDisabled = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -63,27 +76,27 @@ class _LoginPageState extends State<LoginPage> {
             SizedBox(
                 width: MediaQuery.of(context).size.width - 40,
                 child: TextField(
-                  controller: _emailController,
+                    controller: _emailController,
                     decoration: const InputDecoration(
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(20))),
-                  labelText: 'E-mail',
-                ))),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20))),
+                      labelText: 'E-mail',
+                    ))),
             const SizedBox(
               height: 20,
             ),
             SizedBox(
                 width: MediaQuery.of(context).size.width - 40,
                 child: TextField(
-                  controller: _passwordController,
-                  obscureText: true,
+                    controller: _passwordController,
+                    obscureText: true,
                     enableSuggestions: false,
                     autocorrect: false,
                     decoration: const InputDecoration(
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(20))),
-                  labelText: 'Password',
-                ))),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20))),
+                      labelText: 'Password',
+                    ))),
             Align(
               alignment: Alignment.centerRight,
               child: Padding(
@@ -103,26 +116,41 @@ class _LoginPageState extends State<LoginPage> {
                 width: MediaQuery.of(context).size.width - 40,
                 child: TextButton(
                   style: TextButton.styleFrom(
-                    backgroundColor: Colors.blue,
+                    backgroundColor:
+                        _isLoginDisabled ? Colors.grey : Colors.blue,
                     shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(10)),
                     padding: const EdgeInsets.all(15),
                   ),
-                  onPressed: () async {
-                    var email = _emailController.text;
-                    var password = _passwordController.text;
-                    var loginRes = await _login(email, password);
-                    var loginResJson = json.decode(loginRes!);
-                    if(loginResJson.containsKey("token")) {
-                      _navigate(HomeModule.routeRaiz, HomeModule.route);
-                    } else {
-                      displayDialog(context, "Login error", loginResJson["message"]);
-                    }
-                  },
-                  child: const Text(
-                    "Login",
-                    style: TextStyle(color: Colors.white),
-                  ),
+                  onPressed: _isLoginDisabled
+                      ? null
+                      : () async {
+                          _toggleLoginButton();
+                          var email = _emailController.text;
+                          var password = _passwordController.text;
+                          var loginRes = await _login(email, password);
+                          var loginResJson = json.decode(loginRes!);
+                          if (loginResJson.containsKey("token")) {
+                            _toggleLoginButton();
+                            _navigate(HomeModule.routeRaiz, HomeModule.route);
+                          } else {
+                            _toggleLoginButton();
+                            displayDialog(context, "Login error",
+                                loginResJson["message"]);
+                          }
+                        },
+                  child: _isLoginDisabled
+                      ? const SizedBox(
+                          height: 20,
+                          width: 20,
+                          child: CircularProgressIndicator(
+                            color: Colors.white,
+                          ),
+                        )
+                      : const Text(
+                          "Login",
+                          style: TextStyle(color: Colors.white),
+                        ),
                 )),
             const SizedBox(
               height: 20,
