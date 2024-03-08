b81fc144345810f65dcbf99524ced0a0432fd236
Rafael Costa <44804@tjmt.jus.br>
2024-03-08 14:59:26 -0400

    feat: changed signup page

--- lib/features/signup/signup_page.dart
+++ lib/features/signup/signup_page.dart
@@ -21,6 +21,12 @@ class _SignupPageState extends State<SignupPage> {
     });
   }
 
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController =
+      TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
   late bool _isSignupDisabled;
 
   @override
@@ -29,22 +35,23 @@ class _SignupPageState extends State<SignupPage> {
         body: Center(
             child:
                 Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-      const Text("Signup Page", style: TextStyle(
-        fontSize: 30,
-        fontWeight: FontWeight.bold,
-        color: Colors.blue
-      ),),
+      const Text(
+        "Signup Page",
+        style: TextStyle(
+            fontSize: 30, fontWeight: FontWeight.bold, color: Colors.blue),
+      ),
       const SizedBox(
         height: 110,
       ),
       Form(
+        key: _formKey,
           child: Column(
         children: [
           SizedBox(
             width: MediaQuery.of(context).size.width - 40,
             child: TextFormField(
               decoration: const InputDecoration(
-                border: OutlineInputBorder(),
+                border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(20))),
                 hintText: 'Enter your email',
               ),
             ),
@@ -56,7 +63,7 @@ class _SignupPageState extends State<SignupPage> {
             width: MediaQuery.of(context).size.width - 40,
             child: TextFormField(
               decoration: const InputDecoration(
-                border: OutlineInputBorder(),
+                border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(20))),
                 hintText: 'Password',
               ),
             ),
@@ -68,7 +75,7 @@ class _SignupPageState extends State<SignupPage> {
             width: MediaQuery.of(context).size.width - 40,
             child: TextFormField(
               decoration: const InputDecoration(
-                border: OutlineInputBorder(),
+                border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(20))),
                 hintText: 'Confirm your password',
               ),
             ),
@@ -76,7 +83,20 @@ class _SignupPageState extends State<SignupPage> {
           const SizedBox(
             height: 30,
           ),
-          ElevatedButton(onPressed: () {}, child: _isSignupDisabled ? const CircularProgressIndicator() : const Text('Signup'))
+          SizedBox(
+              width: MediaQuery.of(context).size.width - 250,
+              child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor:
+                        _isSignupDisabled ? Colors.grey : Colors.blue,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    padding: const EdgeInsets.all(15),
+                  ),
+                  onPressed: () {},
+                  child: _isSignupDisabled
+                      ? const CircularProgressIndicator()
+                      : const Text('Signup', style: TextStyle(color: Colors.white),)))
         ],
       ))
     ])));
